200141100dd108d65b5badb22ed2e6335d77370e
Valentin Pisani <valentin.pisani@hotmail.com>
2023-10-05 09:26:29 -0300

    css added

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import InformationComponent from './components/InformationComponent.vue';
 
 <template>
   <header class="row">
-    <div class="col-6">
+    <div class="col-4 informationContainer">
       <InformationComponent />
       <div class="wrapper">
         <nav>
@@ -14,9 +14,10 @@ import InformationComponent from './components/InformationComponent.vue';
         </nav>
       </div>
     </div>
-    <div class="col-6 routerContainer">
-      Descripcion
-      <RouterView />
+    <div class="col-8 routerContainer">
+      <div class="">
+        <RouterView />
+      </div>
     </div>
   </header>
 </template>
@@ -28,16 +29,32 @@ import InformationComponent from './components/InformationComponent.vue';
   box-sizing: border-box;
 }
 
+.informationContainer {
+  padding: 15px;
+  display: flex;
+  justify-content: center;
+}
+
+.routerContainer>div {
+  height: 100%;
+  width: 100%;
+  background: rgb(0, 0, 0);
+  border-radius: 15px;
+  border:2px solid rgb(255, 227, 69);
+  padding: 20px;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
+  background: rgb(0, 0, 0);
+  background: linear-gradient(52deg, rgba(0, 0, 0, 1) 3%, rgba(30, 7, 57, 1) 27%, rgba(118, 0, 255, 1) 100%, rgba(49, 11, 94, 1) 100%);
 }
 
 .routerContainer {
-  background: white;
-  height: 60vh;
-  margin-top: 10vh;
-  width: 60%;
-}
+  height: 70vh;
+  border-radius: 10px;
+  padding: 15px;
 
+}
 </style>
