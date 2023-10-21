9e388591d9ce42a5cc448a4bf9c1742a995ea006
PisaniValentin <valentin.pisani@hotmail.com>
2023-10-21 19:07:02 -0300

    socialMedia button finished

--- src/components/SocialMediaComponent.vue
+++ src/components/SocialMediaComponent.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="contenedor">
-        <div class="circulo"></div>
-        <div class="circulo1"></div>
-        <div class="circulo2"></div>
+        <div class="circulo">
+            <ion-icon name="logo-linkedin"></ion-icon>
+        </div>
+        <div class="circulo1">
+            <ion-icon name="logo-github"></ion-icon>
+        </div>
+        <div class="circulo2">
+            <ion-icon name="mail-outline"></ion-icon>
+        </div>
     </div>
-
-
     <svg xmlns="http://www.w3.org/2000/svg">
         <clipPath id="clip">
             <path d="M 100 0 A 100 100 0 0 1 186.6 150 L 100 100 L 100 0 Z" fill="#000000" />
@@ -26,104 +30,126 @@
     height: 500px;
 }
 
+ion-icon {
+    width: 38px;
+    height: 38px;
+    z-index: 4;
+    position: absolute;
+    left: 125px;
+    top: 54px;
+    transform: scaleX(-1);
+    border-radius: 100%;
+    border: none;
+}
+
+
 .circulo {
     position: absolute;
     width: 300px;
     height: 300px;
     clip-path: url('#clip');
     border: none !important;
-    left: -99px;
-    background: rgb(210, 160, 216);
-    transform: scaleX(-1);
-    z-index: 2;
+    left: -100px;
+    background: #1e161f;
+    transform: scaleX(-1) translateZ(0);
+    z-index: 3 !important;
     transition: 0.5s;
+
 }
 
-.circulo::before {
+.circulo::after {
     content: '';
     position: absolute;
     width: 300px;
     height: 300px;
-    top:-20px;
+    left: -6px;
+    top: -4px;
+    transform: scaleX(0.92) scaleY(0.92) translateZ(0);
+    z-index: 3 !important;
+    background: linear-gradient(238deg, rgba(15, 0, 17, 1) 0%, rgba(215, 215, 215, 1) 100%);
+    -webkit-clip-path: url('#clip');
     clip-path: url('#clip');
-    background-color: rgb(96, 9, 105);
-    transform: scaleX(0.75) scaleY(0.75);
-    border: none !important;
-    z-index: 2 !important;
-
 }
 
-
 .circulo:hover {
-    background: yellow;
-    left: -105px;
-    top: -4px;
+    background: rgb(118, 18, 231);
+    left: -108px;
+    top: -5px;
 }
 
 .circulo1 {
     position: absolute;
     width: 300px;
     height: 300px;
+    -webkit-clip-path: url('#clip');
     clip-path: url('#clip');
     border: none;
-    top: -32px;
+    top: -31px;
     left: -118px;
-    transform: rotateZ(120deg);
-    background: rgb(210, 160, 216);
-    z-index: 1;
+    transform: rotate(120deg) translateZ(0);
+    ;
+    background: #1e161f;
+    z-index: 2;
     transition: 0.5s;
 }
 
-.circulo1::before {
+.circulo1::after {
     content: '';
     position: absolute;
     width: 300px;
     height: 300px;
+    left: -6px;
+    top: -2.3px;
+    transform: scaleX(0.92) scaleY(0.92)translateZ(0);
+    z-index: 2 !important;
+    background: linear-gradient(238deg, rgba(15, 0, 17, 1) 0%, rgba(215, 215, 215, 1) 100%);
+    -webkit-clip-path: url('#clip');
     clip-path: url('#clip');
-    background-color: rgb(96, 9, 105);
-    transform: scaleX(0.75) scaleY(0.75);
-    z-index: 2;
-    left: -1px;
-    top: -17px;
 
 }
 
 .circulo1:hover {
-    background: yellow;
-    top: -26px;
+    background: rgb(118, 18, 231);
+    top: -23px;
 }
 
 .circulo2 {
     position: absolute;
     width: 300px;
     height: 300px;
+    -webkit-clip-path: url('#clip');
     clip-path: url('#clip');
     border: none !important;
-    background: rgb(210, 160, 216);
-    transform: scaleX(1);
-    z-index: 0;
+    background: #1e161f;
+    transform: scaleX(1) translateZ(0);
+    z-index: 1 !important;
     transition: 0.5s;
     top: 0px;
     left: 0px;
 }
 
-
-.circulo2::before {
+.circulo2::after {
     content: '';
     position: absolute;
     width: 300px;
     height: 300px;
+    left: -6px;
+    top: -4px;
+    transform: scaleX(0.92) scaleY(0.92) translateZ(0);
+    z-index: 0 !important;
+    background: linear-gradient(238deg, rgba(15, 0, 17, 1) 0%, rgba(215, 215, 215, 1) 100%);
+    -webkit-clip-path: url('#clip');
     clip-path: url('#clip');
-    background: rgb(96, 9, 105);
-    transform: scaleX(0.75) scaleY(0.75);
-    z-index: 2;
-    top:-20px;
 }
 
 .circulo2:hover {
-    background: yellow;
+    background: rgb(118, 18, 231);
     top: -5px;
     left: 8px;
+}
+
+.circulo1 ion-icon {
+    transform: rotate(240deg);
 }
 </style>
 
